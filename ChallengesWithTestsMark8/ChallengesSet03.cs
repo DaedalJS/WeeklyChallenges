4e44fd6144696445da9bb5c49b6bfee1c7045446
Josh Strawbridge <HolyKnightJoshua@gmail.com>
2020-11-18 20:16:58 -0800

    Week 3 Challenges done

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,47 +8,85 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            foreach (bool x in vals)
+            {
+                if (x) { } else { return true; }
+            }
+            return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            
+            return numbers == null ? false : numbers.Where(x => x % 2 != 0).Sum() % 2 != 0 ? true : false;
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+
+            if (password == "" || password == null)
+            {
+                return false;
+            } 
+            else 
+            { 
+
+                var contNums = false;
+                var contUpps = false;
+                var contLower = false;
+                var alpha = "abcdefghijklmnopqrstuvwxyz";
+                var numerals = "0123456789";
+                foreach (char x in password)
+                {
+                    if (numerals.Contains(x)) { contNums = true; }
+                    if (alpha.Contains(x)) { contLower = true; }
+                    if (alpha.ToUpper().Contains(x)) { contUpps = true; }
+                }
+                return contUpps && contNums && contLower ? true : false;
+            }
+
+            
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            char[] valArray = val.ToCharArray();
+
+            return valArray[0];
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            char[] valArray = val.ToCharArray();
+
+            return valArray[valArray.Length - 1];
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            return divisor == 0 ? 0 : dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums[nums.Length-1] - nums[0];
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+             int[] odds = new int[] {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99}; 
+                        
+            return odds;
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            int i = 0;
+            foreach (string word in words)
+            {
+                words[i] = word.ToUpper();
+                i++;
+            }
         }
     }
 }
